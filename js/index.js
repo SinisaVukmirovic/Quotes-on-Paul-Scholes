f373533dd5d0d91e0607bf023f4e8f5c42b1082a
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-01-08 18:30:45 +0100

    Added randomize functionality and destructured data

--- js/index.js
+++ js/index.js
@@ -1,24 +1,33 @@
 const generateBtnElem = document.querySelector('#getQuote');
 const appElem = document.querySelector('#app');
 
-const jsonData = '../data/quotes.json';
+const jsonDataUrl = '../data/quotes.json';
+
+const getRandomQuoteNumb = (dataAmount) => {
+    const randomQuoteNumb = Math.floor(Math.random() * dataAmount);
+    return randomQuoteNumb;
+};
 
 async function getQuote() {
-    const response = await fetch(jsonData);
+    const response = await fetch(jsonDataUrl);
     const data = await response.json();
-    // const { avatar, author, title, quote } = data;
-    // console.log(data[33].avatar);
-    const avatar = document.createElement('img');
+
+    const dataAmount = data.length;
+
+    const avatarElem = document.createElement('img');
+
+    let randomQuoteNumb = getRandomQuoteNumb(dataAmount);
 
     // 1st way
     // avatar.setAttribute('src', data[33].avatar);
     // avatar.setAttribute('alt', 'Profile image of the author');
     // 2nd way
-    avatar.src = data[33].avatar;
-    avatar.alt = 'Profile image of the author';
-    
+    const { avatar, author, title, quote } = data[randomQuoteNumb];
+    console.log(title);
+    avatarElem.src = avatar;
+    avatarElem.alt = 'Profile image of the author';
     // avatar.alt = `${data[33].avatar}`;
-    appElem.appendChild(avatar);
+    appElem.appendChild(avatarElem);
 }
 
 generateBtnElem.addEventListener('click', getQuote);
