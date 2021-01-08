9f0024694799dfef671d552202bdeb551a4d7faf
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-01-08 21:45:11 +0100

    Display functionality added

--- js/index.js
+++ js/index.js
@@ -3,31 +3,43 @@ const appElem = document.querySelector('#app');
 
 const jsonDataUrl = '../data/quotes.json';
 
-const getRandomQuoteNumb = (dataAmount) => {
-    const randomQuoteNumb = Math.floor(Math.random() * dataAmount);
-    return randomQuoteNumb;
-};
-
-async function getQuote() {
+const getQuote = async () => {
     const response = await fetch(jsonDataUrl);
     const data = await response.json();
 
-    const dataAmount = data.length;
+    findOutTheAmountOfData(data);
+
+    // const dataAmount = data.length;
+
+    // let randomQuoteNumb = getRandomQuoteNumb(dataAmount);
+    
+    // displayQuoteElem(data, randomQuoteNumb);
+};
+
+const findOutTheAmountOfData = (data) => {
+    const amountOfData = data.length;
+
+    let randomQuoteNumb = getRandomQuoteNumb(amountOfData);
+    
+    displayQuoteElem(data, randomQuoteNumb);
+};
+
+const getRandomQuoteNumb = (amountOfData) => {
+    
+    // let randomQuoteNumb = Math.floor(Math.random() * amountofData);
+    return Math.floor(Math.random() * amountOfData);
+};
+
+const displayQuoteElem = (data, randomQuoteNumb) => {
+    const { avatar, author, title, quote } = data[randomQuoteNumb];
 
     const avatarElem = document.createElement('img');
 
-    let randomQuoteNumb = getRandomQuoteNumb(dataAmount);
-
-    // 1st way
-    // avatar.setAttribute('src', data[33].avatar);
-    // avatar.setAttribute('alt', 'Profile image of the author');
-    // 2nd way
-    const { avatar, author, title, quote } = data[randomQuoteNumb];
-    console.log(title);
-    avatarElem.src = avatar;
-    avatarElem.alt = 'Profile image of the author';
+    // console.log(quote);
+    avatarElem.setAttribute('src', avatar);
+    avatarElem.setAttribute('alt', 'Profile image of the author');
     // avatar.alt = `${data[33].avatar}`;
     appElem.appendChild(avatarElem);
-}
+};
 
 generateBtnElem.addEventListener('click', getQuote);
